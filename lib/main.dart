a642eea5dd7eea7b957ac9af82baabd73bf13e8b
Oluwatoni <emmanueltoni307@gmail.com>
2025-03-31 11:36:03 +0100

    Updated application to fix issues

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: '/log',
+      initialRoute: '/',
       routes: {
         '/': (context) => WaterTrackScreen(),
         '/splash': (context) => SplashScreen(),
